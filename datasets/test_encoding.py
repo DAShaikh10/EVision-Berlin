7188dfe1b0924de46a31b9afb551fe62d5870b08
Danish Ali <D.A.Shaikh10@gmail.com>
2025-12-24 08:41:20 +0100

    v2.0 - Move `datasets` folder per DDD architecture.

--- datasets/test_encoding.py
+++
@@ -1,7 +0,0 @@
-# import the chardet library
-import chardet
-
-# use the detect method to find the encoding
-# 'rb' means read in the file as binary
-with open("datasets/Ladesaeulenregister.csv", 'rb') as file:
-    print(chardet.detect(file.read()))
