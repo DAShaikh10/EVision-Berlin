87b50fe8ef5531596deee9585cfee6b80c6c0eff
Danish Ali <D.A.Shaikh10@gmail.com>
2025-12-24 08:44:15 +0100

    v2.0 - Update `config.py`paths

--- config.py
+++ config.py
@@ -1,11 +1,10 @@
 p = dict()
 
-p["dataset_folder"] = "datasets"
-p["picklefolder"] = "pickles"
+p["dataset_folder"] = "src/shared/infrastructure/datasets"
 
 p["geocode"] = "PLZ"
 
-p["file_lstations"] = "Ladesaeulenregister.csv" # Original file name: Ladesaeulenregister_BNetzA_2025-10-23.csv
+p["file_lstations"] = "Ladesaeulenregister.csv"  # Original file name: Ladesaeulenregister_BNetzA_2025-10-23.csv
 p["file_residents"] = "plz_einwohner.csv"
 
 p["file_geodat_plz"] = "geodata_berlin_plz.csv"
